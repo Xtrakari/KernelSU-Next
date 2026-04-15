f50bc2299cca365d617736216c8d01ae2c867720
holzip <104773229+holzip@users.noreply.github.com>
2026-04-15 21:45:42 +0300

    fix: backport scheduler header for legacy kernel compatibility

--- kernel/selinux/rules.c
+++ kernel/selinux/rules.c
@@ -5,8 +5,12 @@
 #include <linux/version.h>
 #include <linux/slab.h>
 #include <linux/string.h>
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 12, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 13, 0)
+#include <uapi/linux/sched/types.h>
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(4, 12, 0)
 #include <linux/sched/types.h>
+#else
+#include <linux/sched.h>
 #endif
 #include <linux/stop_machine.h>
 
